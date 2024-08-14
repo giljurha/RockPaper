ba044db296c934e45e6978a0152f521f83c760b0
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-08-15 01:43:04 +0900

    Feat: 기능추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Box from "./component/Box"
 
@@ -24,15 +24,40 @@ const choice = {
   }
 }
 function App() {
+  const [userSelect, setUserSelect] = useState(null)
+  const [computerSelect, setComputerSelect] = useState(null)
+  const [result, setResult] = useState("")
+  
+
   const play = (userChoice) => {
-    console.log("선택됨!", userChoice);
+    setUserSelect(choice[userChoice])
+    let computerChoice = randomChoice()
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice))
+    
+  }
+
+  const judgement = (user, computer) => {
+    if (user.name == computer.name) {
+      return "tie"
+    } else if(user.name == "Rock") return computer.name == "Scissors" ? "win" : "lose"
+    else if (user.name == "Scissors") return computer.name == "Paper" ? "win" : "lose"
+    else if (user.name == "Paper") return computer == "Rock" ? "win" : "lose";
+  }
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random()*itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
   }
 
   return (
     <div>
       <div className='main'>
-      <Box title="you"/>
-      <Box title="computer"/>
+      <Box title="You" item={userSelect} result={result}/>
+      <Box title="Computer" item={computerSelect} result={result}/>
     </div>
       <div className='main'>
         <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const Box = (props) => {
+  console.log("props", props)
   return (
     <div className="box">
       <h1>{props.title}</h1>
       <img
         className="item-img"
-        src="https://assets.katogroup.eu/i/katogroup/VT8-0919-24_02_victorinox"
+        src={props.item && props.item.img}
       />
 
-      <h2>WIN</h2>
+      <h2>{props.result}</h2>
     </div>
   );
 };
